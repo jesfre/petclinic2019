c056905c8c9e5ab07b7f9a8513e119078ee98920
Jorge Ruiz-Aquino <jesfre.gy@gmail.com>
2019-02-03 06:35:16 -0500

    Initial commit.

---
+++ src/main/java/com/blogspot/jesfre/petclinic2019/Petclinic2019Application.java
@@ -0,0 +1,14 @@
+package com.blogspot.jesfre.petclinic2019;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Petclinic2019Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Petclinic2019Application.class, args);
+    }
+
+}
+

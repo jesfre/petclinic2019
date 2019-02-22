85c6275c77cfa19b93ce965c8eccf38d7ee5ec27
Jorge Ruiz-Aquino <jesfre.gy@gmail.com>
2019-02-22 01:14:07 -0500

    Added basic Services for all entities. Closes #11

---
+++ petclinic2019-data/src/main/java/com/blogspot/jesfre/petclinic2019/services/jpa/PetTypeJpaService.java
@@ -0,0 +1,47 @@
+package com.blogspot.jesfre.petclinic2019.services.jpa;
+
+import com.blogspot.jesfre.petclinic2019.model.PetType;
+import com.blogspot.jesfre.petclinic2019.repositories.PetTypeRepository;
+import com.blogspot.jesfre.petclinic2019.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class PetTypeJpaService implements PetTypeService {
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeJpaService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> petTypes = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypes::add);
+        return petTypes;
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return petTypeRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petTypeRepository.deleteById(id);
+    }
+}
---
+++ petclinic2019-data/src/main/java/com/blogspot/jesfre/petclinic2019/services/jpa/SpecialityJpaService.java
@@ -0,0 +1,47 @@
+package com.blogspot.jesfre.petclinic2019.services.jpa;
+
+import com.blogspot.jesfre.petclinic2019.model.Speciality;
+import com.blogspot.jesfre.petclinic2019.repositories.SpecialityRepository;
+import com.blogspot.jesfre.petclinic2019.services.SpecialityService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class SpecialityJpaService implements SpecialityService {
+    private final SpecialityRepository specialityRepository;
+
+    public SpecialityJpaService(SpecialityRepository specialityRepository) {
+        this.specialityRepository = specialityRepository;
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialities = new HashSet<>();
+        specialityRepository.findAll().forEach(specialities::add);
+        return specialities;
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return specialityRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return specialityRepository.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        specialityRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        specialityRepository.deleteById(id);
+    }
+}

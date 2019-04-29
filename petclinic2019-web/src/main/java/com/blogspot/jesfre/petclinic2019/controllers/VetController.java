5cf0978e5aff9f261d6acbca9f472782cf9266bb
Jorge Ruiz-Aquino <jesfre.gy@gmail.com>
2019-04-29 02:20:18 -0400

    Updated the list view of vets. Closes #18

--- petclinic2019-web/src/main/java/com/blogspot/jesfre/petclinic2019/controllers/VetController.java
+++ petclinic2019-web/src/main/java/com/blogspot/jesfre/petclinic2019/controllers/VetController.java
@@ -1,12 +1,17 @@
 package com.blogspot.jesfre.petclinic2019.controllers;
 
+
+import com.blogspot.jesfre.petclinic2019.model.Vet;
 import com.blogspot.jesfre.petclinic2019.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
-@RequestMapping("/vets")
 public class VetController {
 
     private final VetService vetService;
@@ -15,9 +20,17 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"", "/", "/home", "/home.html"})
-    public String vetList(Model model) {
-        model.addAttribute("vetList", vetService.findAll());
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
+    public String listVets(Model model){
+
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/vets";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson(){
+
+        return vetService.findAll();
     }
 }
